b25606f2d43aa3c9f7bb7c5ef59b4b08f48382ad
Jesus Andres Bernal Lopez <jesusandresbernallopez@Jesuss-MBP-4.attlocal.net>
2018-09-09 11:26:02 -0700

    Completed required and optional

---
+++ Tumblr-Feed/PhotoCell.swift
@@ -0,0 +1,34 @@
+//
+//  PhotoCell.swift
+//  Tumblr-Feed
+//
+//  Created by Jesus Andres Bernal Lopez on 9/7/18.
+//  Copyright © 2018 Jesus Andres Bernal Lopez. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var photoCellImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath as IndexPath) as! PhotoCell
+        return cell
+    }
+    
+
+
+
+}
---
+++ Tumblr-Feed/PhotosViewController.swift
@@ -0,0 +1,77 @@
+//
+//  PhotosViewController.swift
+//  Tumblr-Feed
+//
+//  Created by Jesus Andres Bernal Lopez on 9/7/18.
+//  Copyright © 2018 Jesus Andres Bernal Lopez. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var posts : [[String: Any]] = []
+    var refreshFeed = UIRefreshControl()
+    
+    
+    @IBOutlet weak var photoFeedTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        photoFeedTableView.delegate = self
+        photoFeedTableView.dataSource = self
+        
+        refreshFeed.addTarget(self, action: #selector(didPulltoRefresh), for: .valueChanged)
+        photoFeedTableView.insertSubview(refreshFeed, at: 0)
+        
+        fetchFeed()
+    }
+    
+    @objc func didPulltoRefresh(){
+        fetchFeed()
+    }
+    
+    func fetchFeed(){
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data,
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                // Get the dictionary from the response key
+                let responseDictionary = dataDictionary["response"] as! [String: Any]
+                
+                // Store the returned array of dictionaries in our posts property
+                self.posts = responseDictionary["posts"] as! [[String: Any]]
+                self.photoFeedTableView.reloadData()
+                self.photoFeedTableView.reloadData()
+                self.refreshFeed.endRefreshing()
+            }
+        }
+        task.resume()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
+        let post = posts[indexPath.row]
+        if let photos = post["photos"] as? [[String: Any]]{
+            let photo = photos[0]
+            let origionalSize = photo["original_size"] as! [String: Any]
+            let urlString = origionalSize["url"] as! String
+            let url = URL(string: urlString)
+            cell.photoCellImageView.af_setImage(withURL: url!)
+        }
+        
+        return cell
+    }
+    
+
+
+}
